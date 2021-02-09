008ea65581fd39e08fe6f63f3aea5f2158df2f78
Soo-Beom <soobeomkim77@gmail.com>
2021-02-08 20:09:28 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const name = ["Lisa", "Kaitlin", "Jan"]
+
+function writeCards(name, event) {
+    let cards = [];
+    event = 'write' ;
+    for (let n = 0; n < name.length; n++) {
+        cards[n] = (`Thank you, ${name[n]}, for the wonderful surprise gift!`);
+        debugger;
+    }
+
+    return cards;
+}
+
+let number = 5
+function countDown(number) {
+    while (number >=0) {
+        console.log(number--);
+    }
+}
